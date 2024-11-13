ca71b560d5d5dc6f2e92486a63ce80a486e697c8
Harshad Zagade <Hzagade21@gmail.com>
2024-11-13 17:07:11 +0530

    new pages updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,69 @@ const activeElem = function () {
 }
 
 addEventOnElem(window, "scroll", activeElem);
+
+
+// SLider
+
+
+$('.custom-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+ responsive: [
+    {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+    {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+    {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
+});
+
+// Accordion
+$(document).ready(function() {
+  //toggle the component with class accordion_body
+  $(".accordion_head").click(function() {
+    $(this).removeClass('coll-back');
+    if ($('.accordion_body').is(':visible')) {
+      $(".accordion_body").slideUp(300);
+      $(".plusminus").text('+');
+      $(this).removeClass('coll-back');
+      $('.rmv-cls').removeClass('coll-back');
+    }
+
+    if($(this).next(".accordion_body").is(':visible')) {
+      $(this).next(".accordion_body").slideUp(300);
+      $(this).children(".plusminus").text('+');
+      $(this).removeClass('coll-back');
+    }else {
+      $(this).next(".accordion_body").slideDown(300);
+      $(this).children(".plusminus").text('');
+      $(this).children(".plusminus").append('<hr class="hr-clc">');
+      $(this).toggleClass('coll-back');
+      $(this).addClass('rmv-cls');
+    }
+  });
+});
