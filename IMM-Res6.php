8de9b57ed69fed64717fcefa01c7e4d28b02e5c1
Harshad Zagade <Hzagade21@gmail.com>
2025-01-17 14:45:35 +0530

    feat: add Microsoft Clarity tracking script to multiple files and clean up whitespace

--- IMM-Res6.php
+++ IMM-Res6.php
@@ -33,7 +33,22 @@
                 f.parentNode.insertBefore(j, f);
             })(window, document, 'script', 'dataLayer', 'GTM-PWD7ZBXF');
         </script>
-        <!-- End Google Tag Manager -->
+            <!-- End Google Tag Manager -->
+
+    <!-- Microsoft Clarity -->
+    <script type="text/javascript">
+        (function(c, l, a, r, i, t, y) {
+            c[a] = c[a] || function() {
+                (c[a].q = c[a].q || []).push(arguments)
+            };
+            t = l.createElement(r);
+            t.async = 1;
+            t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "pv5l42udd7");
+    </script>
+    <!-- End Microsoft Clarity -->
 
 
         <!-- bootstrap css cdn -->
